c0d461e7a208cddb07568676ff9f3eb8de69fac4
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-31 15:41:29 -0500

    Update: Foreach to display complete items

--- index.php
+++ index.php
@@ -28,13 +28,14 @@ echo '<th>Priority</th>';
 echo '<th>Due Date</th>';
 echo '<th>Complete</th>';
 echo '</th>';
-foreach($list as $item){//Item becomes associative array with named key and value
+foreach($filter as $id){//Change array to $filter and $item to $id because it identifies list item
     echo '<tr>';//Each iteration add table row
     echo /*Each iteration add table cell */ '<td>' . $item['title'] . "</td> \n";//Echo value of 'title' on each iteration
-    echo '<td>' . $item['priority'] . "<td> \n";
-    echo '<td>' . $item['due'] . "<td> \n";
+    echo '<td>' . $list[$id]['title'] . "<td> \n";//Reference list directly through its indexed $id
+    echo '<td>' . $list[$id]['priority'] . "<td> \n";
+    echo '<td>' . $list[$id]['due'] . "<td> \n";
     echo '<td>'; 
-    if ($item['complete']){ //Add condtional true/false complete/incomplete 
+    if ($list[$id]['complete']){ //Add condtional true/false complete/incomplete 
            echo 'Yes';//Print yes if true
     } else {
         echo 'No';//Print no if false      
